18343ec038204e00d3bb137db325e6af2e3c5d32
AnnFCakaSirMom <annflyget.ac@gmail.com>
2026-04-19 13:55:50 +0200

    feat: add collection of SVG icon components to icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -44,5 +44,7 @@ export const ImportIcon: React.FC<IconProps> = (props) => <Icon {...props}><path
 export const Copy: React.FC<IconProps> = (props) => <Icon {...props}><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></Icon>;
 export const CopyIcon: React.FC<IconProps> = Copy;
 export const CheckIcon: React.FC<IconProps> = (props) => <Icon {...props}><path d="M20 6 9 17l-5-5" /></Icon>;
+export const Check: React.FC<IconProps> = CheckIcon;
+export const Link: React.FC<IconProps> = (props) => <Icon {...props}><path d="M10 13l4-4" /><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" /><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" /></Icon>;
 export const Square: React.FC<IconProps> = (props) => <Icon {...props}><rect x="3" y="3" width="18" height="18" rx="2" ry="2" /></Icon>;
 export const Database: React.FC<IconProps> = (props) => <Icon {...props}><ellipse cx="12" cy="5" rx="9" ry="3"/><path d="M3 5V19A9 3 0 0 0 21 19V5"/><path d="M3 12A9 3 0 0 0 21 12"/></Icon>;
